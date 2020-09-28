6f6c83bd9b8d82e6f25432a35eff7b123537ee74
Daniel Lim <lahuman@daum.net>
2020-09-28 23:18:33 +0900

    feat : md5 hash 알고리즘을 이용하여 파일 중복 체크 고도화

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-
 const fs = require('fs');
 const glob = require('glob');
-var path = require("path");
+const path = require("path");
+const md5File = require('md5-file')
 
 
 const fileInfos = {};
+const reduplication = [];
+const searchPahth = process.argv[2];
+if (!searchPahth) {
 
-glob("./**/*.js", { mark: true, realpath: true }, function (er, files) {
+    console.log("usage : node index.js {searchpath}{searchFileType}");
+    console.log("node index.js ./searchPath/**/*.txt");
 
+    return;
+}
+
+
+glob(searchPahth, {  matchBase: true, nodir: true, mark: true, realpath: true, dot: true }, function (er, files) {
+    // console.log(files)
     files.forEach(f => {
+        // console.log(f)
+        // console.log(fs.lstatSync(f).isDirectory())
+        // if(fs.lstatSync(f).isDirectory()) return true;
         const file = path.basename(f);
-        if (!fileInfos[file]) {
-            fileInfos[file] = [];
+        const hash = md5File.sync(f);
+
+        if (!fileInfos[hash]) {
+            fileInfos[hash] = [];
         }
 
-        fileInfos[file].push({ filepath: f });
+        fileInfos[hash].push({ filepath: f, hash, fileName: file });
     });
     for (let i in fileInfos) {
         if (fileInfos[i].length > 1) {
-            console.log(fileInfos[i])
+            reduplication.push(fileInfos[i]);
         }
     }
-})
+
+    if (reduplication.length === 0) {
+        console.log('No duplicate files!');
+    } else {
+        console.log(JSON.stringify(reduplication, null, 4));
+    }
+});
+
+
+
 
 
 // print process.argv
